5bb3e646d578d137b3739950e561d49dc1f91024
Tony Bowden <tony@tmtm.com>
2018-08-01 11:20:31 +0100

    update scraper towards modern style

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,13 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'nokogiri'
-require 'colorize'
 require 'pry'
+require 'scraped'
+require 'scraperwiki'
+
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-
-class String
-  def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
-  end
-end
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
@@ -23,13 +18,14 @@ def scrape_list(url)
   table = noko.xpath('//h2[span[@id="Members"]]/following-sibling::table[1]') or raise "No table"
   table.css('tr').drop(1).each do |tr|
     td = tr.css('td')
-    data = { 
-      name: td[0].text,
+    data = {
+      name: td[0].text.tidy,
       wikiname: td[0].xpath('a[not(@class="new")]/@title').text,
-      constituency: td[1].text,
+      constituency: td[1].text.tidy,
       party: 'unknown',
       term: 9,
     }
+    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
     ScraperWiki.save_sqlite([:name, :constituency, :term], data)
   end
 end
